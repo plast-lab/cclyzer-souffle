ca98a0c49621b5de9325e805388dcc71f3d5d027
Christos Zissis <chrzissis@di.uoa.gr>
2018-11-21 14:51:56 +0200

    Pre-processing big ints now works with non-fixed fact folder

--- cti.py
+++ cti.py
@@ -1,17 +1,20 @@
 import csv
 import os
+import sys
 
+folder=sys.argv[1]
+if not(folder.endswith('/')):
+    folder=folder+'/'
+os.rename(folder+'constant_to_int.csv', folder+'constant_to_int.csv.bak')
 
-os.rename('facts/constant_to_int.csv', 'facts/constant_to_int.csv.bak')
-
-csv_in = open('facts/constant_to_int.csv.bak', 'rb')
-csv_out = open('facts/constant_to_int.csv', 'wb')
+csv_in = open(folder+'constant_to_int.csv.bak', 'rb')
+csv_out = open(folder+'constant_to_int.csv', 'wb')
 
 
 writer = csv.writer(csv_out, delimiter='\t')
 for row in csv.reader(csv_in, delimiter='\t'):
     number = row[1]
-    if ((int(number) <=  9223372036854775807) & (int(number) >= -9223372036854775807)):
+    if ((int(number) <=  2147483647) & (int(number) >= -2147483647)):
         writer.writerow(row)
 
 csv_in.close()
